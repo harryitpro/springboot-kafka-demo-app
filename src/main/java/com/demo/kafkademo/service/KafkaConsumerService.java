338785f6f90f93ddf78b39becaa89bff27432441
Harry Xu <harry.xv@gmail.com>
2025-04-16 22:21:48 -0400

    add KafkaConfig to handle multiple broker and consumers

--- src/main/java/com/demo/kafkademo/service/KafkaConsumerService.java
+++ src/main/java/com/demo/kafkademo/service/KafkaConsumerService.java
@@ -3,11 +3,15 @@ package com.demo.kafkademo.service;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class KafkaConsumerService {
 
+    private final String consumerId = UUID.randomUUID().toString();
+
     @KafkaListener(topics = "demo-topic", groupId = "demo-group")
     public void consume(String message) {
-        System.out.println("Received message: " + message);
+        System.out.println("Consumer [" + consumerId + "] received message: " + message);
     }
 }
